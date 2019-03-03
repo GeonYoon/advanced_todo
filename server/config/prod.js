6c98787d3adf44758b03a302d4f20933be044897
geon yoon <geonyoon@1x-nat-vl931-172-30-130-207.wireless.umass.edu>
2019-03-03 18:23:47 -0500

    test2

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,14 +1,14 @@
 module.exports = {
-    'googleAuth' : {
-        'clientID' : process.env.GOOGLE_CLIENT_ID,
-        'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL' : '/api/auth/google/callback'
-    },
-    'facebookAuth' : {
-        'clientID'      : process.env.FB_CLIENT_ID, 
-        'clientSecret'  : process.env.FB_CLIENT_SECRET, 
-        'callbackURL'   : '/api/auth/facebook/callback'
-    },
+    // 'googleAuth' : {
+    //     'clientID' : process.env.GOOGLE_CLIENT_ID,
+    //     'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
+    //     'callbackURL' : '/api/auth/google/callback'
+    // },
+    // 'facebookAuth' : {
+    //     'clientID'      : process.env.FB_CLIENT_ID, 
+    //     'clientSecret'  : process.env.FB_CLIENT_SECRET, 
+    //     'callbackURL'   : '/api/auth/facebook/callback'
+    // },
     mongoURI : process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 }
