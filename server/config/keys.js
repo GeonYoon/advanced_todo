46ccf6b57f78d22949324d9ac51ce6a6a7b62d9a
geon yoon <geonyoon@1x-nat-vl931-172-30-130-207.wireless.umass.edu>
2019-03-03 17:48:14 -0500

    debugging

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,7 @@
 // keys.js - figure out what set of credentials to return 
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
+    console.log("I'm here")
     module.exports = require("./prod")
 }else {
     // we are in development - return the dev keys
