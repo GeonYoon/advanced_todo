3e4de88e6bb02299271e6c9cc7c0e7440285cfb9
Geon Yoon <ssy01013@gmail.com>
2019-08-31 22:45:06 -0400

    update landing page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import TodoDashList from './TodoDashList'
 
+
 const Dashboard = () => {
     return(
         <div>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import gif from './assets/todo_gif.gif'
+import './css/image.css'
+
 
 const Landing = () => {
     return (
+
         <div style ={{ textAlign : 'center'}}>
-            <h1>
-                Todo!
-            </h1>
-                Organize your stuffs to do 
+            <div className="heading"><i>Each day I will accomplish one thing on my to do list</i></div>
+            <img src={gif} border="0" className="responsive-image" alt="Null" />
+            <div className="text">You're one click away from making your life better</div>
         </div>
     );
 };
