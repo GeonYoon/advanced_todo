0d1e8d9f6f5c21d4db860a2c89a339c6d4a23fab
Geon Yoon <ssy01013@gmail.com>
2019-09-02 12:51:31 -0400

    back + plus api

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientID' : process.env.GOOGLE_CLIENT_ID,
         'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL' : 'http://todo.geonyoon.com/api/auth/google/callback'
+        'callbackURL' : '/api/auth/google/callback'
     },
     'facebookAuth' : {
         'clientID'      : process.env.FB_CLIENT_ID, 
