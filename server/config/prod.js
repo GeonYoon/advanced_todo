a682dbc1ad4b4fc4a9c9ee7f78f361c4ea8fd882
Geon Yoon <ssy01013@gmail.com>
2019-09-02 13:18:19 -0400

    replace call back url

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientID' : process.env.GOOGLE_CLIENT_ID,
         'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL' : '/api/auth/google/callback'
+        'callbackURL' : 'http://todo.geonyoon.com'
     },
     'facebookAuth' : {
         'clientID'      : process.env.FB_CLIENT_ID, 
