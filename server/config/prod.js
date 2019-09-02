6ea82723058b65f77183eb7818776eb0deeb4f8c
Geon Yoon <ssy01013@gmail.com>
2019-09-02 13:44:15 -0400

    back

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,12 +2,12 @@ module.exports = {
     'googleAuth' : {
         'clientID' : process.env.GOOGLE_CLIENT_ID,
         'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL' : 'http://todo.geonyoon.com'
-    },
+        'callbackURL' : '/auth/google/callback'
+    }, 
     'facebookAuth' : {
         'clientID'      : process.env.FB_CLIENT_ID, 
         'clientSecret'  : process.env.FB_CLIENT_SECRET, 
-        'callbackURL'   : '/api/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     },
     mongoURI : process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
