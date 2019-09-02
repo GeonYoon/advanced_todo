cfbfe2bc0dc26a980ba5a155610974e454897909
Geon Yoon <ssy01013@gmail.com>
2019-09-02 11:22:55 -0400

    take it back

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,12 +2,12 @@ module.exports = {
     'googleAuth' : {
         'clientID' : process.env.GOOGLE_CLIENT_ID,
         'clientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL' : 'ec2-3-130-47-223.us-east-2.compute.amazonaws.com/auth/google/callback'
+        'callbackURL' : '/auth/google/callback'
     },
     'facebookAuth' : {
         'clientID'      : process.env.FB_CLIENT_ID, 
         'clientSecret'  : process.env.FB_CLIENT_SECRET, 
-        'callbackURL'   : 'ec2-3-130-47-223.us-east-2.compute.amazonaws.com/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     },
     mongoURI : process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
